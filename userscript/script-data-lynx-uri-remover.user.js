af3ca3c36ad08f584d803e4d44b3d972749a3b34
Adam Chao <adam@thinkaliker.com>
2021-02-19 20:59:24 -0800

    update to add fixed link nearby, better detection

--- userscript/script-data-lynx-uri-remover.user.js
+++ userscript/script-data-lynx-uri-remover.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         data-lynx-uri remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Removes data-lynx-uri from FB Messenger links (original answer from https://superuser.com/a/1318222)
+// @version      0.2
+// @description  Removes FB tracking from FB/Messenger links (original answer from https://superuser.com/a/1318222)
 // @author       stachu (modified by thinkaliker)
 // @match        https://www.messenger.com/*
-// @match        https://www.facebook.com/*
+// @match        https://www.facebook.com/messages/*
 // @grant        none
 // ==/UserScript==
 
@@ -14,10 +14,19 @@
     document.addEventListener('mouseover', event => {
         let target = event.target;
 
-        if (target.tagName.toLowerCase() === "a") {
-            Array.from(target.attributes).filter(attribute => attribute.name.startsWith("data-lynx-uri")).forEach(attribute => target.removeAttribute(attribute.name));
-            console.log('Removed tracker')
+        if (target.hasAttribute('role') && !target.hasAttribute('aria-label')){
+            if (target.tagName.toLowerCase() === "a" && target.getAttribute('role') === "link") {
+                Array.from(target.attributes).filter(attribute => attribute.name.startsWith("data-lynx-")).forEach(attribute => target.removeAttribute(attribute.name));
+                target.onClick = function() {return false;};
+                if (!target.href.includes('l.messenger.com') && !target.href.includes('l.facebook.com')) {
+                    target.insertAdjacentHTML('afterend', '&nbsp;&nbsp;<span onClick="window.open(\'' + target.href + '\', \'_blank\'\)" style="font-weight:bold;font-style:italic;text-decoration:underline double;span:hover\{\}">FIXED LINK</span>' );
+                    target.setAttribute('href', '#');
+                    target.setAttribute('role', '');
+                    console.log('Removed tracker here');
+                }
+            }
         }
-    })
+    });
+
 
 })();
